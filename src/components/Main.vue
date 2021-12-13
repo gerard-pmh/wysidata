72451e13125ce713533614425ee90d4d31dbdac7
Pierre Gerard <gerard.pmh@gmail.com>
2021-12-13 23:59:13 +0100

    hide wysiwyg ui when sharing

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="grid fixed grid-cols-4 w-full font-sans text-gray-50">
-    <component-panel />
+    <component-panel v-if="showWysiwygUi" />
 
-    <div class="col-span-2 bg-gray-700">
+    <div
+      class="bg-gray-700"
+      :class="showWysiwygUi ? 'col-span-2' : 'col-span-4'"
+    >
       <div class="flex justify-end">
         <button
           class="px-2 m-1 rounded-full bg-stone-100 text-stone-800 font-medium"
@@ -14,7 +17,7 @@
       <wysiwyg-area />
     </div>
 
-    <api-panel />
+    <api-panel v-if="showWysiwygUi" />
   </div>
 </template>
 
@@ -22,7 +25,7 @@
 import ApiPanel from './ApiPanel/ApiPanel.vue';
 import ComponentPanel from './ComponentPanel/ComponentPanel.vue';
 import WysiwygArea from './WysiwygArea/WysiwygArea.vue';
-import { effect, onMounted } from 'vue';
+import { effect, onMounted, ref } from 'vue';
 import { useStore } from '../store';
 import { useRoute } from 'vue-router';
 
@@ -30,11 +33,13 @@ const route = useRoute();
 const store = useStore();
 
 let base64StateLoaded = false;
+let showWysiwygUi = ref(true);
 effect(() => {
   if (!base64StateLoaded) {
     const base64State = route.params.base64State;
     if (base64State) {
       store.dispatch('loadBase64State', base64State);
+      showWysiwygUi.value = false;
     }
     base64StateLoaded = true;
   }
